e458d3ad9dc269612677c042d45f4e65d4e99c54
OlgaShir <cherokee70@mail.ru>
2020-09-27 14:19:33 +0700

    HW start

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -11,7 +11,7 @@ class CyclesGoldenFibo {
 
     }
 
-    public static int fiboNumber (int n){
+    public static int fiboNumber(int n) {
         if (n == 1 || n == 2) {
             return 1;
         } else {
@@ -19,23 +19,34 @@ class CyclesGoldenFibo {
         }
     }
 
-    public static boolean isGoldenTriangle(int a, int b, int c){
+    public static boolean isGoldenTriangle(int a, int b, int c) {
 
-        if (a == b && (double)a/c > 1.61703 && (double)a/c < 1.61903){
+        if (a == b && (double) a / c > 1.61703 && (double) a / c < 1.61903) {
             System.out.println("золото");
-            return true;}
-        else if (c == b && b/(double)a > 1.61703 && b/(double)a < 1.61903){
+            return true;
+        } else if (c == b && b / (double) a > 1.61703 && b / (double) a < 1.61903) {
             System.out.println("золото");
-            return true;}
-        else if (a == c && (double)c/b > 1.61703 && (double)c/b < 1.61903){
+            return true;
+        } else if (a == c && (double) c / b > 1.61703 && (double) c / b < 1.61903) {
             System.out.println("золото");
-            return true;}
-        else
+            return true;
+        } else
             return false;
-        }
+    }
 
     public static void main(String[] args) {
-        isGoldenTriangle(89, 55, 55);
+        int n1 = 1;
+        int n2 = 1;
+        int fib;
+        System.out.println(n1);
+        System.out.println(n2);
+
+        for (int i = 1; i <= 15; i++) {
+            fib = n1 + n2;
+            n1 = n2;
+            n2 = fib;
+            System.out.println(fib);
+        }
     }
-    }
+}
 
