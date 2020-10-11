630fc0467aa265eb602df09b5f06f7884989ea25
OlgaShir <cherokee70@mail.ru>
2020-10-12 03:33:34 +0700

    HW start

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,6 +1,9 @@
 package ru.progwards.java1.lessons.classes;
 
 public class ComplexNum {
+    int a;
+    int b;
+
     public ComplexNum(int a, int b){
         this.a = a;
         this.b = b;
@@ -11,17 +14,32 @@ public class ComplexNum {
         else
             return a+"-"+ -b+"i";
     }
+
+
     public ComplexNum add(ComplexNum num) {
-        int c;
-        int d;
-        this.c = c;
-        this.d = d;
+        return new ComplexNum(this.a + num.a, this.b + num.b);
     }
 
-        public String toString(){
-            return (a+c)+"+"+(b+d)+"i";
+    public ComplexNum sub(ComplexNum num){
+        int AnsR = this.a - num.a;
+        int AnsI = this.b - num.b;
+        ComplexNum Sum = new ComplexNum(AnsR, AnsI);
+        return Sum;
+    }
 
+    public ComplexNum mul(ComplexNum num) {
+        int AnsI = (this.a * num.b) + (this.b * num.a);
+        int AnsR = (this.a * num.a) - (this.b * num.b);
+        ComplexNum Sum = new ComplexNum(AnsR, AnsI);
+        return Sum;
+    }
+    public ComplexNum div(ComplexNum num)
+    {
+        ComplexNum ComCon = new ComplexNum(num.a, (num.b * -1));
+        ComplexNum part1 = new ComplexNum(this.a, this.b).mul(ComCon);
+        ComplexNum part2 = num.mul(ComCon);
 
+        return part1;
     }
 
     public static void main(String[] args) {
