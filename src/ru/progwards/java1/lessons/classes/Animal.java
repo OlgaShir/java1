0208a696ed66673107d31b32eede29964485d999
OlgaShir <cherokee70@mail.ru>
2020-10-13 02:09:12 +0700

    HW start

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -57,71 +57,7 @@ public class Animal {
     public String toStringFull() {
         return "I am " + getKind() + ", eat " + getFoodKind() + " " + calculateFoodWeight();
     }
-
-
-    class Cow extends Animal {
-
-        public Cow(double weight) {
-            super(weight);
-        }
-
-        @Override
-        public AnimalKind getKind() {
-            return an2;
-        }
-
-        @Override
-        public FoodKind getFoodKind() {
-            return food2;
-        }
-
-        @Override
-        public double getFoodCoeff() {
-            return 0.05;
-        }
-    }
-
-    class Hamster extends Animal {
-        public Hamster(double weight) {
-            super(weight);
-        }
-        @Override
-        public AnimalKind getKind() {
-            return an3;
-        }
-
-        @Override
-        public FoodKind getFoodKind() {
-            return food3;
-        }
-
-        @Override
-        public double getFoodCoeff() {
-            return 0.03;
-        }
-    }
-
-    class Duck extends Animal {
-        public Duck(double weight) {
-            super(weight);
-        }
-
-        @Override
-        public AnimalKind getKind() {
-            return an4;
-        }
-
-        @Override
-        public FoodKind getFoodKind() {
-            return food3;
-        }
-
-        @Override
-        public double getFoodCoeff() {
-            return 0.04;
-        }
-    }
-
+    
     public static void main(String[] args) {
         System.out.println();
         }
