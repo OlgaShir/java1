f92e81b84c8fe0e9ab4883f45c52e58537b2d2b7
OlgaShir <cherokee70@mail.ru>
2020-10-26 01:14:28 +0700

    HW start

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -1,4 +1,18 @@
 package ru.progwards.java1.lessons.arrays;
 
 public class ArraySort {
+    public static void sort(int[] a){
+        for(int i = a.length-1 ; i > 0 ; i--){
+            for(int j = 0 ; j < i ; j++){
+                if( a[j] > a[j+1] ){
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
