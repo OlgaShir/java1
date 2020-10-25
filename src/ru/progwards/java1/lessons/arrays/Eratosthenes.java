25663d0cd91316d3cbe680f65f3122509c81b27b
OlgaShir <cherokee70@mail.ru>
2020-10-26 00:48:48 +0700

    HW start

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -19,7 +19,19 @@ public class Eratosthenes {
                 }
             }
         }
-
-    public static void main(String[] args) {
+        public boolean isSimple(int n){
+        if (n < 2){
+            return false;
+        }
+        for (int i = 2; i < sieve.length; i++){
+            if (n%i == 0) {
+                return false;
+            }
+        }
+    return true;
     }
-}
+    public static void main (String[]args){
+        Eratosthenes N = new Eratosthenes(99);
+        System.out.println(N.isSimple(7));
+    }
+    }
