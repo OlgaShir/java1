524aceba8e73dd012117ab141a4c9abd51a3329b
OlgaShir <cherokee70@mail.ru>
2020-10-28 02:50:08 +0700

    HW start

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -12,7 +12,30 @@ public class DIntArray {
      int[] massivPlus = Arrays.copyOf(massiv, massiv.length+1);
       massivPlus[massivPlus.length-1] = num;
     }
-
+    public void atInsert(int pos, int num){
+        int[] massivIns = Arrays.copyOf(massiv, massiv.length+1);
+        for (int i = 0; i < massivIns.length; i++){
+      if (i < pos) {
+          massivIns[i] = massiv[i];
+      }  else {
+          massivIns[i+1] = massiv[i];
+      }
+        }
+        massivIns[pos] = num;
+    }
+    public void atDelete(int pos){
+        int[] massivDel = Arrays.copyOf(massiv, massiv.length-1);
+        for (int i = 0; i < massivDel.length; i++){
+            if (i < pos) {
+                massivDel[i] = massiv[i];
+            }  else {
+                massivDel[i-1] = massiv[i];
+            }
+        }
+    }
+    public int at(int pos){
+        return  massiv[pos];
+    }
     public static void main(String[] args) {
 
     }
