c69b73422911e20aa8b7a524b48c2874233ec34c
OlgaShir <cherokee70@mail.ru>
2020-10-30 23:43:06 +0700

    HW start

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -7,14 +7,16 @@ public class DIntArray {
 
     DIntArray(){
         int[] massiv;
+        this.massiv = new int[0];
     }
     public void add(int num){
      int[] massivPlus = Arrays.copyOf(massiv, massiv.length+1);
       massivPlus[massivPlus.length-1] = num;
+      this.massiv = massivPlus;
     }
     public void atInsert(int pos, int num){
         int[] massivIns = Arrays.copyOf(massiv, massiv.length+1);
-        for (int i = 0; i < massivIns.length; i++){
+        for (int i = 0; i < massiv.length; i++){
       if (i < pos) {
           massivIns[i] = massiv[i];
       }  else {
@@ -22,21 +24,24 @@ public class DIntArray {
       }
         }
         massivIns[pos] = num;
+        this.massiv = massivIns;
     }
     public void atDelete(int pos){
-        int[] massivDel = Arrays.copyOf(massiv, massiv.length-1);
+        int[] massivDel = new int[massiv.length - 1];
         for (int i = 0; i < massivDel.length; i++){
             if (i < pos) {
                 massivDel[i] = massiv[i];
             }  else {
-                massivDel[i-1] = massiv[i];
+                massivDel[i] = massiv[i+1];
             }
         }
+        this.massiv = massivDel;
     }
     public int at(int pos){
         return  massiv[pos];
     }
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
+        }
     }
-    }
+
