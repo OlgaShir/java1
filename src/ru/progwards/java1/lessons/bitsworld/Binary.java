682ed6ff5770300176812b8acbffaa08f907f9a8
OlgaShir <cherokee70@mail.ru>
2020-12-07 02:12:16 +0700

    HW start

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -2,15 +2,15 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class Binary {
     public Binary(byte num) {
-        public String toString(){
-            string s = "";
+        public String toString();
+            String s = "";
             while (num > 0){
                 s = ((num % 2) == 0 ? "0" : "1") + s;
                 num = num / 2;
             }
-            return; s;
+            return s;
         }
-    }
+
     public static void main(String[] args){
 
     }
